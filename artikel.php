7ea6a7feed64a892d04e3d00b848dd07c0e5989f
ahmadyulinu <ahamdyulinu@gmail.com>
2020-12-20 15:17:00 +0700

    Beranda dan Detail Artikel

---
+++ artikel.php
@@ -0,0 +1,67 @@
+<?php 
+    include 'config.php';
+    $id = $_GET['id'];
+    $res = $collection->findOne(['id_artikel' => "$id"]);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow rounded" style="word-spacing: 3px;">
+  <a class="navbar-brand" href="#">My Adventure</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="insert.php">Write an Article</a>
+      </li>
+      </ul>
+
+  
+  </div>
+</nav>
+    <div class="container mt-4">
+    <div class="col d-flex justify-content-center">
+        <div class="row">
+            <div class="box mt-5 shadow">
+            <?php if(!empty($res)) { ?>
+                <div class="text-container m-5">
+                    <h4><?= $res['judul_artikel'];?></h4>
+                    <p><?= $res['isi_artikel']; ?></p>
+                <hr>
+                    <div class="float-left mb-3">
+                        <a href="index.php" class="nav-item text-secondary">< home</a>
+                    </div>
+                    <div class="float-right mb-3">
+                    <a href="edit.php?id=<?= $id ;?>" class="text-secondary">< Edit this article</a>
+                </div>
+                </div>
+             <?php   } else { ?>
+                <div class="text-container m-5">
+                    <h4>There is nothing here...</h4>
+                    <p>Go back to where you came from.</p>
+                <hr>
+                <div class="float-left mb-3">
+                    <a href="index.php" class="text-secondary">< home</a>
+                </div>
+
+                
+                </div>
+            <?php }
+            ?>
+            </div> 
+        </div>
+    </div>
+    </div>
+    
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,57 @@
 <?php
 include 'config.php';
 
+/* insert document
 $insertOneResult = $collection->insertOne([
-    'id_artikel' => '2',
-    'judul_artikel' => 'Masak memasak',
-    'isi_artikel' => 'Lorem ipsum',
+    'id_artikel' => '6',
+    'judul_artikel' => $title,
+    'isi_artikel' => 'Lorem ipsum dolor',
 ]);
+*/
 
-printf("Inserted %d document(s)\n", $insertOneResult->getInsertedCount());
+// read document
+$res = $collection ->find();
+?>
 
-var_dump($insertOneResult->getInsertedId());
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow rounded" style="word-spacing: 3px;">
+  <a class="navbar-brand" href="#">My Adventure</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="insert.php">Write an Article</a>
+      </li>
+      </ul>  
+  </div>
+  </nav>
+
+  <div class="container mt-4">
+    <div class="listed-article mt-4">
+            <div class="text text-center">
+              <h3>Daftar Artikel</h3>
+            </div>
+        <?php 
+            foreach ($res as $result) { ?>
+            <div class="card m-4 shadow">
+              <div class="card-body">
+                <?php echo '<h4>', $result['judul_artikel'], '</h4>'; ?>
+                <?php echo '<a href="artikel.php?id=',$result['id_artikel'], '" class="text-secondary">Read article</a>'; ?>
+              </div>
+            </div>
+        <?php } ?>
+  </div>
+  </div>
+</body>
+</html>
